6bd474c45fd1e56660d1f83cfa8d6b5b8cc1f1cc
Lukas Lueg <lukas.lueg@gmail.com>
2017-02-20 20:26:53 +0100

    Make testbox integration tests freestanding.
    Cargo can break dependency cycles between dev-dependencies and
    dependencies but crates.io can't. Start running integration tests in a
    free-standing temporary directory.

--- tests/testbox_tests.rs
+++ tests/testbox_tests.rs
@@ -1,13 +1,117 @@
 #![deny(warnings, bad_style, future_incompatible, unused, missing_docs, unused_comparisons)]
-extern crate testbox;
+extern crate tempdir;
 extern crate time;
 extern crate semver;
 extern crate built;
 
-use testbox::built_info;
+use std::env;
+use std::fs;
+use std::io;
+use std::process;
+use std::io::Write;
+use std::path;
+
+
+struct Project {
+    root: tempdir::TempDir,
+    files: Vec<(path::PathBuf, Vec<u8>)>,
+}
+
+impl Project {
+    fn new() -> Project {
+        Project { root: tempdir::TempDir::new("built_integration").unwrap(),
+                  files: Vec::new() }
+    }
+
+    fn add_file<N: Into<path::PathBuf>, C: Into<Vec<u8>>>(&mut self, name: N, content: C) {
+        self.files.push((name.into(), content.into()))
+    }
+
+    /// Hold on to the tempdir, it will be removed when droped!
+    fn create(self) -> io::Result<tempdir::TempDir> {
+        fs::DirBuilder::new().create(&self.root.path().join("src")).unwrap();
+        for (name, content) in self.files {
+            let fname = self.root.path().join(name);
+            let mut file = fs::File::create(&fname)?;
+            file.write_all(&content)?;
+        }
+        Ok(self.root)
+    }
+}
+
+/// Tries to find built's Cargo.toml, panics if it ends up in /
+fn get_built_root() -> path::PathBuf {
+    env::current_exe()
+         .map(|path| {
+             let mut path = path;
+             loop {
+                 if path.join("Cargo.toml").exists() {
+                     break;
+                 }
+                 path = path::PathBuf::from(path.parent().unwrap());
+             }
+             path
+         }).unwrap()
+}
 
 #[test]
-fn testbox() {
+fn new_testbox() {
+    let mut p = Project::new();
+
+    let built_root = get_built_root();
+
+    p.add_file("Cargo.toml",
+               format!("
+[package]
+name = \"testbox\"
+version = \"1.2.3-rc1\"
+authors = [\"Joe\", \"Bob\", \"Harry:Potter\"]
+build = \"build.rs\"
+description = \"xobtset\"
+homepage = \"localhost\"
+
+[dependencies]
+time = \"0.1\"
+semver = \"0.6\"
+built = {{ path = {:?}, features=[\"serialized_git\", \"serialized_time\", \"serialized_version\"] }}
+
+[build-dependencies]
+built = {{ path = {:?}, features=[\"serialized_git\", \"serialized_time\", \"serialized_version\"] }}
+
+[features]
+default = [\"SuperAwesome\", \"MegaAwesome\"]
+SuperAwesome = []
+MegaAwesome = []", &built_root, &built_root));
+
+    p.add_file("build.rs", r#"
+use std::env;
+use std::path;
+extern crate built;
+
+fn main() {
+    // Teleport to a CI-platform, should get detected
+    env::set_var("CONTINUOUS_INTEGRATION", "1");
+
+    let mut options = built::Options::default();
+    options.set_dependencies(true);
+    let src = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let dst = path::Path::new(&env::var("OUT_DIR").unwrap()).join("built.rs");
+    built::write_built_file_with_opts(&options, &src, &dst).unwrap();
+}"#);
+
+    p.add_file("src/main.rs", r#"
+//! The testbox.
+#![deny(warnings, bad_style, future_incompatible, unused, missing_docs, unused_comparisons)]
+
+extern crate built;
+extern crate semver;
+extern crate time;
+
+mod built_info {
+    include!(concat!(env!("OUT_DIR"), "/built.rs"));
+}
+
+fn main() {
     assert_eq!(built_info::GIT_VERSION, None);
     assert!(built_info::CI_PLATFORM.is_some());
     assert_eq!(built_info::PKG_VERSION, "1.2.3-rc1");
@@ -30,16 +134,26 @@ fn testbox() {
     assert_ne!(built_info::RUSTC_VERSION, "");
     assert_ne!(built_info::RUSTDOC_VERSION, "");
     assert_ne!(built_info::DEPENDENCIES_STR, "");
-}
+    assert_ne!(built_info::HOST, "");
+    assert_ne!(built_info::TARGET, "");
+    assert_ne!(built_info::RUSTC, "");
+    assert_ne!(built_info::RUSTDOC, "");
 
-#[test]
-fn parse_dependencies() {
     let v = built_info::DEPENDENCIES;
     assert!(built::util::parse_versions(&v)
         .any(|(name, ver)| name == "cmake" && ver >= semver::Version::parse("0.1.0").unwrap()));
-}
 
-#[test]
-fn built_time() {
     assert!((built::util::strptime(built_info::BUILT_TIME_UTC) - time::now()).num_days() <= 1);
+}"#);
+
+    let root = p.create().expect("Creating the project failed");
+    let cargo_result = process::Command::new("cargo")
+                                .current_dir(&root)
+                                .arg("run")
+                                .arg("-q")
+                                .output()
+                                .expect("cargo failed");
+    if ! cargo_result.status.success() {
+        panic!("cargo failed with {}", String::from_utf8_lossy(&cargo_result.stderr));
+    }
 }
