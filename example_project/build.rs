78d3dc5d607c540106d2809492ef0e1b55d286c2
Lukas Lueg <lukas.lueg@gmail.com>
2020-01-24 19:56:33 +0100

    Fix example project

--- example_project/build.rs
+++ example_project/build.rs
@@ -1,14 +1,9 @@
-extern crate built;
-
-use std::env;
-use std::path;
-
 fn main() {
     let mut opts = built::Options::default();
     opts.set_dependencies(true);
 
-    let src = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let dst = path::Path::new(&env::var("OUT_DIR").unwrap()).join("built.rs");
-    built::write_built_file_with_opts(&opts, &src, &dst)
+    let src = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    let dst = std::path::Path::new(&std::env::var("OUT_DIR").unwrap()).join("built.rs");
+    built::write_built_file_with_opts(&opts, src.as_ref(), &dst)
         .expect("Failed to acquire build-time information");
 }
